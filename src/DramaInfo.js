763337e7549d8634aebeeb89c21906b3a7489e09
yoon sang seok (kyle) <amamov@kakao.com>
2022-05-14 19:15:48 +0900

    refactor : 코드 최적화

--- src/DramaInfo.js
+++ src/DramaInfo.js
@@ -1,29 +1,54 @@
-import React from "react"
-import { useState } from "react"
-import "./DramaInfo.css"
+import React, { useState } from "react";
+import { Styles } from "./DramaInfo.style";
+import "./DramaInfo.css";
 
-const DramaInfo = () => {
-    const [title, setTitle] = useState("");
-    const [year, setYear] = useState(0);
-    const [information, setInformation] = useState("")  
+export default function DramaInfo({
+  id,
+  videoType,
+  title,
+  likeCount,
+  createdDate,
+  dueDate,
+  poster,
+  description,
+  handleLikeClick,
+}) {
+  const [isShow, setIsShow] = useState(false);
 
-    const onChangeTitle = (event) =>{
-    setTitle(event.target.value);
-    }
-    const onChangeYear = (event) =>{
-    setYear(event.target.value);
-    }
-    const onChangeIntro = (event) =>{
-    setInformation(event.target.value);
-    }
+  const handleShowClick = () => {
+    setIsShow((prev) => !prev);
+  };
 
-    return(
-        <div className="Show">
-        <h2> {title} </h2>
-        <p> {year} </p>
-        <p> {information} </p>
+  return (
+    <>
+      <div className="ArticleList">
+        <Styles.VideoTitle>
+          <span onClick={handleShowClick}>{`${videoType}, ${title}`}</span>
+          <span className="Good" onClick={() => handleLikeClick(id)}>
+            👍🏻
+          </span>
+          {likeCount}
+        </Styles.VideoTitle>
+        <p>{createdDate}</p>
+        <hr />
       </div>
-    )
-}
 
-export {DramaInfo};
+      {isShow && (
+        <div className="Show">
+          <h2>{title}</h2>
+          <p>{dueDate}</p>
+          {poster ? <img className="PosterImage" src={poster} alt="img" /> : ""}
+
+          <p>
+            작품 소개
+            <br />
+            <br />
+            {`"${description.first}${description.second === "" ? `"` : ""}`}
+            <br />
+            {description.second === "" ? "" : `${description.second}"`}
+          </p>
+        </div>
+      )}
+    </>
+  );
+}
